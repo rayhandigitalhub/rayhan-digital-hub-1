b0116ed6c096f407df32cca69ace50a9bdd7c70d
rayhandigitalhub <rayhandigitalhub@gmail.com>
2026-07-22 15:54:17 +0600

    Update siteConfig.js

--- src/siteConfig.js
+++ src/siteConfig.js
@@ -13,7 +13,7 @@ export const siteConfig = {
   brand: {
     name: "Rayhan Ahmed",
     business: "Rayhan Digital Hub",
-    initials: "/src/components/MagneticLogo.jsx",
+    initials: "/profesinal-DP.png",
     roles: [
       "SEO Expert",
       "Digital Marketing Specialist",
